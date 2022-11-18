f5eccbdb08c7b3999d248f757c54053353b19ee7
angie2512 <a20191792@pucp.edu.pe>
2022-11-18 00:00:34 -0500

    servletcliente1

--- src/main/java/com/example/lab10/Servlets/ServletContract.java
+++ src/main/java/com/example/lab10/Servlets/ServletContract.java
@@ -1,14 +1,23 @@
 package com.example.lab10.Servlets;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import com.example.lab10.Daos.ContratoDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "ServletContract", value = "/ServletContract")
 public class ServletContract extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String action = request.getParameter("accion") == null?"listar":request.getParameter("accion");
+        ContratoDao contratoDao = new ContratoDao();
+        jakarta.servlet.RequestDispatcher view;
+
 
     }
 
